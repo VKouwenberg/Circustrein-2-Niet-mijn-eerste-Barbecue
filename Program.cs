423eb6920a08840797111ba1898f2b9ef46f2411
McFinnigan <v.kouwenberg@student.fontys.nl>
2023-09-22 18:23:42 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Circustrein_2_Classes.Classes;
+using Circustrein_2_Classes.Enums;
+using Circustrein_2_Classes;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        AnimalManager.EnterAllAnimals();
+    }
+}
